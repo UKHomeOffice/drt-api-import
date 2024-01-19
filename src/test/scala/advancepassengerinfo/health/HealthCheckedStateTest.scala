bcfe847be3a4e644ee0a7a4d190cbf82dd6c28ca
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2024-01-19 12:43:36 +0000

    DRTII-1058 update names for test
    Signed-off-by: Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>

--- src/test/scala/advancepassengerinfo/health/HealthCheckedStateTest.scala
+++ src/test/scala/advancepassengerinfo/health/HealthCheckedStateTest.scala
@@ -10,15 +10,15 @@ import scala.concurrent.duration.DurationInt
 class HealthCheckedStateTest extends AnyFlatSpec with Matchers {
 
   "HealthCheckedState" should "return true for hasCheckedSince, if the lastCheckedAt is within the last 5 minutes" in {
-    val processState = HealthCheckedState()
-    processState.hasCheckedSince(5.minutes) shouldBe true
+    val healthCheckedState = HealthCheckedState()
+    healthCheckedState.hasCheckedSince(5.minutes) shouldBe true
   }
 
   it should "return false for hasCheckedSince, if the lastCheckedAt is more than 5 minutes ago" in {
-    val processState = HealthCheckedState()
+    val healthCheckedState = HealthCheckedState()
     val sixMinutesAgo = Instant.now().minus(6, ChronoUnit.MINUTES)
-    processState.setLastCheckedAt(sixMinutesAgo)
-    processState.hasCheckedSince(5.minutes) shouldBe false
+    healthCheckedState.setLastCheckedAt(sixMinutesAgo)
+    healthCheckedState.hasCheckedSince(5.minutes) shouldBe false
   }
 
 }
