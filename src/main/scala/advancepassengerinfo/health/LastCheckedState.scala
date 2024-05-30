f35323747f7ec6a15b786baee1b5394fe5b35582
Rich Birch <ringo@albumsnaps.com>
2024-05-30 09:37:32 +0100

    DRTII-1499 Add logging to health check function

--- src/main/scala/advancepassengerinfo/health/LastCheckedState.scala
+++ src/main/scala/advancepassengerinfo/health/LastCheckedState.scala
@@ -1,17 +1,25 @@
 package advancepassengerinfo.health
 
 import drtlib.SDate
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration.{DurationLong, FiniteDuration}
 
 case class LastCheckedState(now: () => SDate) {
+  private val log = LoggerFactory.getLogger(getClass)
   private var lastCheckedAt: Option[SDate] = None
 
   def hasCheckedSince(threshold: FiniteDuration): Boolean =
     lastCheckedAt
       .exists { lca =>
         val lastCheckedAgo = (now().millisSinceEpoch - lca.millisSinceEpoch).millis
-        lastCheckedAgo <= threshold
+
+        val checkedWithinThreshold = lastCheckedAgo <= threshold
+
+        if (!checkedWithinThreshold)
+          log.warn(s"Last checked at $lca, which was $lastCheckedAgo ago, which is more than the threshold of $threshold")
+
+        checkedWithinThreshold
       }
 
   def setLastCheckedAt(at: SDate): Unit = lastCheckedAt = Some(at)
