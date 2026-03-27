00dee529c95784ac782ad373302f80c4babda1c4
Scala Steward <scala_steward@virtuslab.com>
2026-03-27 02:59:39 +0000

    Update scalatest to 3.2.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ object Dependencies {
   lazy val pekkoVersion = "1.1.3"
   lazy val pekkoHttpVersion = "1.1.0"
 
-  val scalatestVersion = "3.2.19"
+  val scalatestVersion = "3.2.20"
   val typesafeConfigVersion = "1.4.3"
   val awsSdkS3Version = "2.30.2"
   val jodaTimeVersion = "2.12.7"
